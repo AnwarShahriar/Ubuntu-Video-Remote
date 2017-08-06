5b0210b9d1b3449e1f85319763c0005707fed999
anwarshahriar <anwarshahriar@gmail.com>
2017-08-07 00:45:11 +0600

    Cache IP address through android app restart

--- uvr-android/app/src/main/java/me/anwarshahriar/uvr/MainActivity.kt
+++ uvr-android/app/src/main/java/me/anwarshahriar/uvr/MainActivity.kt
@@ -2,11 +2,13 @@ package me.anwarshahriar.uvr
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.preference.PreferenceManager
 import android.widget.Button
 import android.widget.EditText
 import com.androidnetworking.AndroidNetworking
 
 class MainActivity : AppCompatActivity() {
+  val IP_ADDR_KEY = "KEY_IP_ADDR"
 
   val toggleVideoButton: Button by lazy { findViewById<Button>(R.id.toggle_video_button) }
   val ipAddressField: EditText by lazy { findViewById<EditText>(R.id.ip_address_field) }
@@ -21,5 +23,24 @@ class MainActivity : AppCompatActivity() {
           .build()
           .getAsString(null);
     }
+
+    ipAddressField.setText(getSavedIP())
+  }
+
+  override fun onPause() {
+    super.onPause()
+    saveIP(ipAddressField.text.toString())
+  }
+
+  private fun saveIP(IP: String) {
+    PreferenceManager.getDefaultSharedPreferences(applicationContext)
+        .edit()
+        .putString(IP_ADDR_KEY, IP)
+        .apply()
+  }
+
+  private fun getSavedIP(): String? {
+    return PreferenceManager.getDefaultSharedPreferences(applicationContext)
+        .getString(IP_ADDR_KEY, null);
   }
 }
